cf2d1c2dcfd72a234a1605915f3404e0fd2efeb8
unknown <info@archistico.com>
2019-06-06 17:28:28 +0200

    init

---
+++ CasualitàProbabilistica/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace CasualitàProbabilistica
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbRis = new System.Windows.Forms.TextBox();
+            this.btCrea = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.upLunghezza = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.upLunghezza)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbRis
+            // 
+            this.tbRis.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.tbRis.Location = new System.Drawing.Point(12, 124);
+            this.tbRis.Multiline = true;
+            this.tbRis.Name = "tbRis";
+            this.tbRis.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbRis.Size = new System.Drawing.Size(268, 409);
+            this.tbRis.TabIndex = 0;
+            // 
+            // btCrea
+            // 
+            this.btCrea.Location = new System.Drawing.Point(12, 71);
+            this.btCrea.Name = "btCrea";
+            this.btCrea.Size = new System.Drawing.Size(268, 47);
+            this.btCrea.TabIndex = 2;
+            this.btCrea.Text = "Crea nuova parola";
+            this.btCrea.UseVisualStyleBackColor = true;
+            this.btCrea.Click += new System.EventHandler(this.btCrea_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Inserisci la lunghezza della parola";
+            // 
+            // upLunghezza
+            // 
+            this.upLunghezza.Location = new System.Drawing.Point(183, 45);
+            this.upLunghezza.Maximum = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+            this.upLunghezza.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.upLunghezza.Name = "upLunghezza";
+            this.upLunghezza.Size = new System.Drawing.Size(97, 20);
+            this.upLunghezza.TabIndex = 4;
+            this.upLunghezza.Value = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(255, 33);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "NUOVE PAROLE";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 545);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.upLunghezza);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btCrea);
+            this.Controls.Add(this.tbRis);
+            this.Name = "frmMain";
+            this.Text = "Casualità Probabilistica";
+            ((System.ComponentModel.ISupportInitialize)(this.upLunghezza)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbRis;
+        private System.Windows.Forms.Button btCrea;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown upLunghezza;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CasualitàProbabilistica/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CasualitàProbabilistica
+{
+    public partial class frmMain : Form
+    {
+        string[] lettere;
+        Dictionary<Tuple<char, char>, double> tabella = new Dictionary<Tuple<char, char>, double>();
+
+        public frmMain()
+        {
+            InitializeComponent();
+            CaricaDati();
+        }
+
+        private void CaricaDati()
+        {
+            string listaLettere = "a;à;b;c;d;e;è;é;f;g;h;i;ì;j;k;l;m;n;o;ò;p;q;r;s;t;u;ù;v;w;x;y;z;#";
+            lettere = listaLettere.Split(';');
+
+            using (StreamReader sr = File.OpenText("ListaProbabilita.txt"))
+            {
+                string linea;
+                int contLettera = 0;
+
+                while ((linea = sr.ReadLine()) != null)
+                {
+                    string[] probabilitaLettera;
+                    probabilitaLettera = linea.Split(';');
+                    int contProbabilita = 0;
+
+                    foreach (string lettera in lettere)
+                    {
+                        tabella.Add(new Tuple<char, char>(lettere[contLettera][0], lettera[0]), Convert.ToDouble (probabilitaLettera[contProbabilita]));
+                        contProbabilita++;
+                    }
+
+                    contLettera++;
+                }
+            }
+        }
+
+        private char CercaLettera(char c, double perc)
+        {
+            foreach (string lettera in lettere)
+            {
+                if (tabella[new Tuple<char, char>(c, lettera[0])] > perc)
+                    return Convert.ToChar(lettera);
+            }
+
+            return '*';
+        }
+
+        private void btCrea_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            double percPrimaLettera = rnd.Next(10000)/100;
+
+            // La prima lettera corrisponde alle probabilità <#,lettera>
+            string[] parola = new string[Convert.ToByte (upLunghezza.Value)];
+            parola[0] = CercaLettera('#', percPrimaLettera).ToString();
+
+            for (int c = 1; c < Convert.ToByte(upLunghezza.Value); c++)
+            {
+                int perc = rnd.Next(100);
+                string nuovaLettera = "";
+                nuovaLettera = CercaLettera(Convert.ToChar(parola[c - 1]), perc).ToString();
+
+                if (nuovaLettera == "#")
+                    break;
+
+                parola[c] = nuovaLettera;
+            }
+
+            tbRis.Text += string.Join("", parola) + System.Environment.NewLine; 
+        }
+    }
+}
